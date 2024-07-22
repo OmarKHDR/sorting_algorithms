ee7bf036963350a8966c5fdb3cd53da0ef6a0700
OmarKHDR <omarsliman37@gmail.com>
2024-07-22 10:37:56 +0300

    adding 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,57 @@
 #include "sort.h"
-
+/**
+* swap - swap
+* @a: pointer
+* @b: another one
+* Description: a function for swAping
+*/
 void swap(int *a, int *b)
 {
-    *a = (*a) ^ (*b);
-    *b = (*a) ^ (*b);
-    *a = (*a) ^ (*b);
+	*a = (*a) ^ (*b);
+	*b = (*a) ^ (*b);
+	*a = (*a) ^ (*b);
 }
-
+/**
+* print_array - print_array
+* @arr: pointer
+* @n: another one
+* Description: a function for printing
+*/
 void print_array(int *arr, size_t n)
 {
-    int i = 0;
-    for(; i < n; i++)
-    {
-        printf("%d",arr[i]);
-        if(i < (n - 1))
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", arr[i]);
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
 
+/**
+* bubble_sort - bubble_sort
+* @array: pointer
+* @size: another one
+* Description: a function for swAping
+*/
 void bubble_sort(int *array, size_t size)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    for(; i < size - 1 ; i++)
-    {
-        for(j = 0 ; j < size - i - 1; j++)
-        {
-            if(array[j] > array[j + 1])
-            {
-                swap(array + j, array + j + 1);
-                print_array(array, size);
-            }
-        }
-    }
+	for (; i < size - 1 ; i++)
+	{
+		for (j = 0 ; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array + j, array + j + 1);
+				print_array(array, size);
+			}
+		}
+	}
 }
 
